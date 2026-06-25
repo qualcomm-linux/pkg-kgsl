0abd33d80f09ccde149f306449d3096e5e21df49
Alexis Cellier <alexis.cellier@canonical.com>
2026-06-25 09:36:22 +0200

    debian: remove kgsl-dkms.service
    The module is already loaded by the soft dependency added though
    modprobe.d configuration file.
    
    Signed-off-by: Alexis Cellier <alexis.cellier@canonical.com>

--- debian/kgsl-load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Called by kgsl-dkms.service on every boot.
-
-# modprobe may segfault after successfully loading msm_kgsl on this platform.
-# The module IS loaded even if modprobe exits non-zero, so always exit 0.
-lsmod | grep -q msm_kgsl || modprobe msm_kgsl
-exit 0
